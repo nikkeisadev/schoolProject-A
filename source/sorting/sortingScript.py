1df6cd68d90c71a2a4e413eab7db2c29947cdd17
nikkeisadev <notnikkecrd@gmail.com>
2023-09-29 18:37:23 +0200

    Bug fixing, :bug:

--- source/sorting/sortingScript.py
+++ source/sorting/sortingScript.py
@@ -10,13 +10,14 @@ os.system(cmd)
 ints = []
 mainList = []
 argValue = None
+freshInject = ''
 preOrderType = None
 questionInject = False
 
 #Asking the user that which option do he/she want's to choose.
 def getOrder():
     global preOrderType
-    preOrderType = str(input('[SORTER]> Welcome! Tell me which option do you want to choose?\n          Descending, or Ascending order? [1/2]> '))
+    preOrderType = str(input('[SORTER]> Welcome! Tell me which option do you want to choose?\n          Ascending or Descending order? [1/2]> '))
 
 #Opening the file, spliting, and placing into the main list.
 def readFile():
@@ -53,6 +54,7 @@ def valuesCheck():
     if argValue:
         for element in mainList:
             ints.append(int(element))
+    print(ints)
 
 #Method for quick ascending sorting. (Only int.)
 def fastOrderAscending(readableList):
@@ -96,29 +98,20 @@ def reverseStringValues(x):
     #Reversing the value, so ascent result will be descent result.
     return x[::-1]
 
-#Shutdown animation
-def shutDown():
-    os.system('cls')
-    shutdownScreen = '''   
+def appendingNewInject():
+    #Globals here.
+    global mainList
+    global questionInject
+    global freshInject
+    global ints
+    #Creating the half reseted enviorment to work with the newly injected item!
+    mainList.clear()
+    ints.clear()
+    readFile()
+    mainList.append(freshInject)
+    valuesCheck() 
+    questionInject = False
 
-
-
-                     _____             __           
-                    / ___/____  ______/ / ____ _____
-                    \__ \/ __ \/  ___/ __/ _ \/ ___/
-                    __/ / /_/  / /  / /_/  __/ /    
-                  /____/\____ /_/   \__/\___/_/     
-    
-
-
-    '''
-    for char in shutdownScreen:
-        sleep(0.001)
-        sys.stdout.write(char)
-        sys.stdout.flush()
-    sleep(1)
-    os.system('cls')
-    exit()
 #Main part of the code, processing every order, as the user wanted.
 def processOrders():
     #Define global values.
@@ -127,6 +120,7 @@ def processOrders():
     global mainList
     global argValue
     global questionInject
+    global freshInject
    #Read this! There are 2 types of orders in this code: the Quick type, and the Bubble method.
    #Qucik sorting used for ints, it's easier for them, and Bubble for only strings.
    #So there is one mentioned sorting type, and one that's did'nt mentioned.
@@ -136,37 +130,37 @@ def processOrders():
             if argValue == False: 
                 #Asking for new item into the list.
                 freshInject = input("[SORTER]> Please type a new item: ")
-                mainlist.delete()
-                readFile()
-                mainList.append(freshInject)
-
-                valuesCheck() 
-                questionInject = False
+                appendingNewInject()
+                print(f'[SORTER]> Added one more item! [{freshInject}]<-----')
+                processOrders()
             else:
                 #Asking for new item into the list.
                 freshInject = input("[SORTER]> Please type a new item: ")
-                mainlist.delete()
-                readFile()
-                mainList.append(freshInject)
-
-                valuesCheck() 
-                questionInject = False
+                appendingNewInject()
+                print(f'[SORTER]> Added one more item! [{freshInject}]<-----')
+                processOrders()
         else:
-            #Exiting if the user chooses no, or press anything else.
-            shutDown()
+            #Reseting the UI, and list.
+            ints.clear()
+            mainList.clear()
+            readFile()
+            valuesCheck()
+            processOrders()
+            
+
     
-    #Ascending or Descending orders are processed here.
+    #Ascending or Descending orders are processed.
     if preOrderType == '1':
         #Fast order process.
         valuesCheck()
         print('[SORTER]> Ascending selected!')
         if argValue is True:
-            print('[SORTER]> Ascending order with Quick Sort method:  ', fastOrderAscending(ints))
+            print('[SORTER]> Ascending order with Quick Sort method:\n         ', fastOrderAscending(ints))
             questionInject = True
             processOrders()
         #Bubble order process.
         elif argValue is False:
-            print('[SORTER]> Ascending order with Bubble Sort method:  ', bubbleSortStringAscent(mainList))
+            print('[SORTER]> Ascending order with Bubble Sort method:\n         ', bubbleSortStringAscent(mainList))
             questionInject = True
             processOrders()
     elif preOrderType == '2':
@@ -174,12 +168,12 @@ def processOrders():
         valuesCheck()
         print('[SORTER]> Descending selected!')
         if argValue is False:
-            print('[SORTER]> Descending order with Bubble Sort method: ', reverseStringValues(bubbleSortStringAscent(mainList)))
+            print('[SORTER]> Descending order with Bubble Sort method:\n         ', reverseStringValues(bubbleSortStringAscent(mainList)))
             questionInject = True
             processOrders()
         #Fast order process.
         elif argValue is True:
-            print('[SORTER]> Descending order with Quick Sort method:', reverseStringValues(fastOrderAscending(ints)))
+            print('[SORTER]> Descending order with Quick Sort method:\n         ', reverseStringValues(fastOrderAscending(ints)))
             questionInject = True
             processOrders()
     else: print(f'Wrong parameter! [{preOrderType}]<----'), getOrder()
