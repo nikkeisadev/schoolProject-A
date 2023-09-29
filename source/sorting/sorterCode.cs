33483d6e55a6ff51bdf45f3f7b21c35ec855d085
nikkeisadev <notnikkecrd@gmail.com>
2023-09-29 08:12:23 +0200

    Okay... So removed Flutter.

---
+++ source/sorting/sorterCode.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+//Importing basic things, such as System, but where is .Collections... I wonder.
+
+namespace sorterCode
+{
+	public class UniverseStub
+	{
+        var logFile = File.ReadAllLines('ki.txt'); //Please place it here if it's possible.
+        var logList = new List<string>(logFile); //Creating the list variable to store fresh informations.
+	}
+}
